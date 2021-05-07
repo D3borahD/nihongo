e66e7151b55823ce96adfac90f2380de6e2f76a6
Deb <deborah.douillet@yahoo.fr>
2021-05-07 09:44:16 +0200

    mise en forme du menu toggle

--- button.js
+++ button.js
@@ -1,7 +1,9 @@
 const headerToggle = document.querySelector(".header__toggle");
 const toggleMenu = document.querySelector(".header__toggle--menu");
+const menu = document.querySelector(".menu");
 
 headerToggle.addEventListener("click", () => {
   toggleMenu.classList.toggle("active");
   headerToggle.classList.toggle("active");
+  menu.classList.toggle("active");
 });
